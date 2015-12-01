3598a8daa1c070ba93f778a4e16261fa59e73b3a
skipsuva <skip.suva@gmail.com>
2015-12-01 09:00:15 -0500

    examine code and shorten some methods

--- hashketball.rb
+++ hashketball.rb
@@ -10,51 +10,51 @@ def game_hash
                                 {:player_name => "Alan Anderson",
                                 :number => 0,
                                 :shoe => 16,
-                                :points => 22, 
-                                :rebounds => 12, 
-                                :assists => 12, 
-                                :steals => 3, 
-                                :blocks => 1, 
+                                :points => 22,
+                                :rebounds => 12,
+                                :assists => 12,
+                                :steals => 3,
+                                :blocks => 1,
                                 :slam_dunks => 1 },
-                                
+
                                 {:player_name => "Reggie Evans",
                                 :number => 30,
                                 :shoe => 14,
-                                :points => 12, 
-                                :rebounds => 12, 
-                                :assists => 12, 
-                                :steals => 12, 
-                                :blocks => 12, 
+                                :points => 12,
+                                :rebounds => 12,
+                                :assists => 12,
+                                :steals => 12,
+                                :blocks => 12,
                                 :slam_dunks => 7},
-                                
+
                                 {:player_name => "Brook Lopez",
                                 :number => 11,
                                 :shoe => 17,
-                                :points => 17, 
-                                :rebounds => 19, 
-                                :assists => 10, 
-                                :steals => 3, 
-                                :blocks => 1, 
+                                :points => 17,
+                                :rebounds => 19,
+                                :assists => 10,
+                                :steals => 3,
+                                :blocks => 1,
                                 :slam_dunks => 15},
-                                
+
                                 {:player_name => "Mason Plumlee",
                                 :number => 1,
                                 :shoe => 19,
-                                :points => 26, 
-                                :rebounds => 12, 
-                                :assists => 6, 
-                                :steals => 3, 
-                                :blocks => 8, 
+                                :points => 26,
+                                :rebounds => 12,
+                                :assists => 6,
+                                :steals => 3,
+                                :blocks => 8,
                                 :slam_dunks => 5},
-                                
+
                                 {:player_name => "Jason Terry",
                                 :number => 31,
                                 :shoe => 15,
-                                :points => 19, 
-                                :rebounds => 2, 
-                                :assists => 2, 
-                                :steals => 4, 
-                                :blocks => 11, 
+                                :points => 19,
+                                :rebounds => 2,
+                                :assists => 2,
+                                :steals => 4,
+                                :blocks => 11,
                                 :slam_dunks => 1}]
         },
         :away => {
@@ -64,55 +64,55 @@ def game_hash
                                 {:player_name => "Jeff Adrien",
                                 :number => 4,
                                 :shoe => 18,
-                                :points => 10, 
-                                :rebounds => 1, 
-                                :assists => 1, 
-                                :steals => 2, 
-                                :blocks => 7, 
+                                :points => 10,
+                                :rebounds => 1,
+                                :assists => 1,
+                                :steals => 2,
+                                :blocks => 7,
                                 :slam_dunks => 2},
-                                
+
                                 {:player_name => "Bismak Biyombo",
                                 :number => 0,
                                 :shoe => 16,
-                                :points => 12, 
-                                :rebounds => 4, 
-                                :assists => 7, 
-                                :steals => 7, 
-                                :blocks => 15, 
+                                :points => 12,
+                                :rebounds => 4,
+                                :assists => 7,
+                                :steals => 7,
+                                :blocks => 15,
                                 :slam_dunks => 10},
-                                
+
                                 {:player_name => "DeSagna Diop",
                                 :number => 2,
                                 :shoe => 14,
-                                :points => 24, 
-                                :rebounds => 12, 
-                                :assists => 12, 
-                                :steals => 4, 
-                                :blocks => 5, 
+                                :points => 24,
+                                :rebounds => 12,
+                                :assists => 12,
+                                :steals => 4,
+                                :blocks => 5,
                                 :slam_dunks => 5},
-                                
+
                                 {:player_name => "Ben Gordon",
                                 :number => 8,
                                 :shoe => 15,
-                                :points => 33, 
-                                :rebounds => 3, 
-                                :assists => 2, 
-                                :steals => 1, 
-                                :blocks => 1, 
+                                :points => 33,
+                                :rebounds => 3,
+                                :assists => 2,
+                                :steals => 1,
+                                :blocks => 1,
                                 :slam_dunks => 0 },
-                                
+
                                 {:player_name => "Brendan Haywood",
                                 :number => 33,
                                 :shoe => 15,
-                                :points => 6, 
-                                :rebounds => 12, 
-                                :assists => 12, 
-                                :steals => 22, 
-                                :blocks => 5, 
+                                :points => 6,
+                                :rebounds => 12,
+                                :assists => 12,
+                                :steals => 22,
+                                :blocks => 5,
                                 :slam_dunks => 12}]
         }
-            
-    }   
+
+    }
     return games
 end
 
@@ -130,7 +130,7 @@ def num_points_scored(player_selection)
     return points_calc(game_hash[:away][:players], player_selection)
   end
 end
-   
+
 
 def shoe_calc(player_array, player_selection)
   player_array.each do |player|
@@ -150,7 +150,7 @@ end
 
 
 def team_colors(team_name)
-    game_hash.each do |location, team_data| 
+    game_hash.each do |location, team_data|
         return team_data[:colors] if team_data[:team_name] == team_name
     end
 end
@@ -158,10 +158,9 @@ end
 def team_names()
     #returns the team names
     name_list = []
-    game_hash.each do |location, team_data|
-        name_list.push(team_data[:team_name])
+    game_hash.collect do |location, team_data|
+        team_data[:team_name]
     end
-    return name_list
 end
 
 
@@ -178,32 +177,16 @@ def player_numbers(team_name)
 end
 
 
-
 def player_stats(player_name)
-    #The stat hash should not include the player's name.
-
-    game_hash.each do |location, team_data|
-        team_data[:players].each do |a|
-            if a[:player_name] == player_name
-                my_hash = a
-                my_hash.shift
-                return my_hash
-            end
-        end
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |a|
+      if a[:player_name] == player_name
+        my_hash = a
+        my_hash.shift
+        return my_hash
+      end
     end
-end
-
-
-def player_stats(player_name)
-    game_hash.each do |location, team_data|
-        team_data[:players].each do |a| 
-            if a[:player_name] == player_name
-                my_hash = a
-                my_hash.shift
-                return my_hash
-            end
-        end
-    end
+  end
 end
 
 def big_shoe_rebounds()
@@ -228,22 +211,3 @@ def big_shoe_rebounds()
     end
     return rebounds
 end
-
-
-def good_practices
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    #binding.pry
-      team_data.each do |attribute, data|
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-        #binding.pry
-
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-        data.each do |data_item|
-            #binding.pry
-      end
-    end
-  end
-end
-
-
