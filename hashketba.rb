40d7b8218b381ab4d10dd850bab458108aed56f7
franco84 <franco84@gmail.com>
2016-01-01 23:39:44 +0000

    Done.

---
+++ hashketba.rb
@@ -0,0 +1,25 @@
+# Write your code here!
+def game_hash
+  game_hash = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black","White"],
+      players:
+      {
+        Alan_Anderson: {Number: "0", Shoe: "16", Points: "22", Rebounds: "12", Assists: "12", Steals: "3", Blocks: "1", Slam_Dunks: "1"},         Reggie_Evans:	{Number: "30", Shoe: "14", Points: "12", Rebounds: "12", Assists: "12", Steals: "12", Blocks: "12", Slam_Dunks: "7"},
+        Brook_Lopez:	{Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""},
+        Mason_Plumlee: {Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""},
+        Jason_Terry:   {Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""}
+        }
+    },
+    away:{
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        Jeff_Adrien:	{Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""},
+        Bismak_Biyombo:{Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""},
+        DeSagna_Diop:	{Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""},
+        Ben_Gordon:	{Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""},
+        Brendan_Haywood:{Number: "", Shoe: "", Points: "", Rebounds: "", Assists: "", Steals: "", Blocks: "", Slam_Dunks: ""}
+        }}}
+end
