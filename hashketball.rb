d1d68d725dfa1592ae0d4d5980c6a8bbe480a7a1
psswf1172 <jennifermabella@gmail.com>
2016-03-29 20:08:45 -0400

    OMG its done

--- hashketball.rb
+++ hashketball.rb
@@ -14,9 +14,9 @@ def game_hash
           :assists => 12,
           :steals => 3,
           :blocks => 1,
-          :slam_dunks => 1
-          },
-          "Reggie Evans" => {
+          :slam_dunks => 1},
+
+          "Reggie Evans" => { 
           :number => 30,
           :shoe => 14,
           :points => 12,
@@ -24,9 +24,9 @@ def game_hash
           :assists => 12,
           :steals => 12,
           :blocks => 12,
-          :slam_dunks => 7
-          },
-         "Brook Lopez" => {
+          :slam_dunks => 7},
+
+          "Brook Lopez" => {
           :number => 11,
           :shoe => 17,
           :points => 17,
@@ -34,18 +34,18 @@ def game_hash
           :assists => 10,
           :steals => 3,
           :blocks => 1,
-          :slam_dunks => 15
-          },
+          :slam_dunks => 15},
+          
           "Mason Plumlee" => {
           :number => 1,
           :shoe => 19,
           :points => 26,
           :rebounds => 12,
-          :assists => 6,
+          :assists => 6, 
           :steals => 3,
           :blocks => 8,
-          :slam_dunks => 5
-          },
+          :slam_dunks => 5},
+          
           "Jason Terry" => {
           :number => 31,
           :shoe => 15,
@@ -54,65 +54,64 @@ def game_hash
           :assists => 2,
           :steals => 4,
           :blocks => 11,
-          :slam_dunks => 1 
-        }
+          :slam_dunks => 1}
       }
     },
            
   :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
-    :players => {  
-        "Jeff Adrien" => {
-        :number => 4,
-        :shoe => 18,
-        :points => 10,
-        :rebounds => 1,
-        :assists => 1,
-        :steals => 2,
-        :blocks => 7,
-        :slam_dunks => 2
-        },
-        "Bismak Biyombo" => {
-        :number => 0,
-        :shoe => 16,
-        :points => 12,
-        :rebounds => 4,
-        :assists => 7,
-        :steals => 7,
-        :blocks => 15,
-        :slam_dunks => 10
-        },
-        "DeSagna Diop" => {
-        :number => 2,
-        :shoe => 14,
-        :points => 24,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 4,
-        :blocks => 5,
-        :slam_dunks => 5
-        },
-        "Ben Gordon" => {
-        :number => 8,
-        :shoe => 15,
-        :points => 33,
-        :rebounds => 3,
-        :assists => 2,
-        :steals => 1,
-        :blocks => 1,
-        :slam_dunks => 0
-        },
-        "Brendan Haywood" => {
-        :number => 33,
-        :shoe => 15,
-        :points => 6,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 22,
-        :blocks => 5,
-        :slam_dunks => 12 
-        }
+    :players =>  { 
+          "Jeff Adrien" => {
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2},
+
+          "Bismak Biyombo" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 7,
+          :blocks => 15,
+          :slam_dunks => 10},
+          
+          "DeSagna Diop" => {
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5},
+          
+          "Ben Gordon" => {
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0},
+          
+          "Brendan Haywood" => {
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 22,
+          :blocks => 5,
+          :slam_dunks => 12}
+        
       }
     }
   }
@@ -120,7 +119,14 @@ end
 
   
   def num_points_scored(player_name)
-    all_players[player_name][:points]
+    scored = nil
+    all_players.each do |player_info|
+      # binding.pry
+      if player_info[0] == player_name
+        scored = player_info[1][:points]
+      end
+    end
+    scored
   end 
 
   def home_players
@@ -171,22 +177,13 @@ end
     brooklyn_numbers = []
     if team_name == away_team[:team_name]
       away_players.each do |player_name, hash_of_stats|
-        player_name.each do |category, stat|
-          if category == :number
-            charlotte_numbers << stat
-          end
+        charlotte_numbers << hash_of_stats[:number]
         end
-      end
       return charlotte_numbers
     else
       home_players.each do |player_name, hash_of_stats|
-        # binding.pry
-        player_name.each do |category, stat|
-          if category == :number
-            brooklyn_numbers << stat
-          end
+        brooklyn_numbers << hash_of_stats[:number]
         end
-      end
       return brooklyn_numbers
     end
   end
@@ -198,8 +195,11 @@ end
 
   def big_shoe_rebounds
     #returns rebounds for player with largest shoes
-    all_players.each do |player|
-      player.shoe_size
-    end
+    sorted = all_players.sort_by { |name, stats| stats[:shoe]}
+    # binding.pry
+    a = sorted.flatten
+    b = sorted.first
+    c = b[1]
+    d = c[:rebounds]
+    return d
   end
-
