8317968d10f5c6209df5b6491d90a882b9c08ace
stonecraftwolf <stonecraftwolf@gmail.com>
2016-07-31 18:25:29 -0400

    for natalie to look at

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,177 @@
 # Write your code here!
 
+require 'pry'
 
+def game_hash
+	{
+		home: {
+			team_name: "Brooklyn Nets",
+			colors: ["Black", "White"],
+			players: {
+				"Alan Anderson" => {
+					number: 0,
+					shoe: 16,
+					points: 22,
+					rebounds: 12,
+					assists: 12,
+					steals: 3,
+					blocks: 1,
+					slam_dunks: 1
+				},
+				
+				"Reggie Evans" => {
+					number: 30,
+					shoe: 14,
+					points: 12,
+					rebounds: 12,
+					assists: 12,
+					steals: 12,
+					blocks: 12,
+					slam_dunks: 7
+				},
+				
+				"Brook Lopez" => {
+					number: 11,
+					shoe: 17,
+					points: 17,
+					rebounds: 19,
+					assists: 10,
+					steals: 3,
+					blocks: 1,
+					slam_dunks: 15
+				},
+				
+				"Mason Plumlee" => {
+					number: 1,
+					shoe: 19,
+					points: 26,
+					rebounds: 12,
+					assists: 6,
+					steals: 3,
+					blocks: 8,
+					slam_dunks: 5
+				},
+				
+				"Jason Terry" => {
+					number: 31,
+					shoe: 15,
+					points: 19,
+					rebounds: 2,
+					assists: 2,
+					steals: 4,
+					blocks: 11,
+					slam_dunks: 1
+				}
+			}
+
+			},
+		away: {
+			team_name: "team name",
+			colors: [],
+			players: {
+				"Jeff Adrien" => {
+					number: 4,
+					shoe: 18,
+					points: 10,
+					rebounds: 1,
+					assists: 1,
+					steals: 2,
+					blocks: 7,
+					slam_dunks: 2
+				},
+
+				"Bismack Biyombo" => {
+					number: 0,
+					shoe: 16,
+					points: 12,
+					rebounds: 4,
+					assists: 7,
+					steals: 7,
+					blocks: 15,
+					slam_dunks: 10
+				},
+
+				"Desagna Diop" => {
+					number: 2,
+					shoe: 14,
+					points: 24,
+					rebounds: 12,
+					assists: 12,
+					steals: 4,
+					blocks: 5,
+					slam_dunks: 5
+				},
+
+				"Ben Gordon" => {
+					number: 8,
+					shoe: 15,
+					points: 33,
+					rebounds: 3,
+					assists: 2,
+					steals: 1,
+					blocks: 1,
+					slam_dunks: 0
+				},
+
+				"Brendan Haywood" => {
+					number: 33,
+					shoe: 15,
+					points: 6,
+					rebounds: 12,
+					assists: 12,
+					steals: 22,
+					blocks: 5,
+					slam_dunks: 12
+				}
+			}
+
+			}
+	}
+
+end
+
+
+def find_player(name)
+	game_hash.each do |team, team_data_types|
+		team_data_types.each do |team_data_types, team_data|
+			if team_data_types == :players
+				team_data.each do |player_name, player_data|
+					if player_name == name
+						player_data
+					end
+				end
+			end
+		end
+	end
+
+
+end
+
+
+def num_points_scored(name)
+	
+	game_hash.each do |team, team_data_types|
+
+		team_data_types.each do |team_data_types, team_data|
+
+			if team_data_types == :players
+
+				team_data.each do |player_name, player_data|
+
+						if player_name == name
+
+
+
+							player_data[:points]	
+binding.pry
+
+						end		
+				end		
+			end
+		end		
+  end
+  
+end
 
 
 
